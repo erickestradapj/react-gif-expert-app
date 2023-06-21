bc21da5aa6a2b52ce1bdeae8fa62147d458edbd7
Erick Estrada <erickestradapj@gmail.com>
2023-06-20 22:05:17 -0500

    feat: list of categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+   console.log(categories);
+
    return (
       <>
          <h1>GifExpertApp</h1>
+
+         <ol>
+            {categories.map((category) => {
+               return (
+                  <>
+                     <li key={category}>{category}</li>
+                  </>
+               );
+            })}
+            {/* <li>ABC</li> */}
+         </ol>
       </>
    );
 };
