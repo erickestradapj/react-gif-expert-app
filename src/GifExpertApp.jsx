8ce898a2ebcb02092d6b16ced64d0c5896874c37
Erick Estrada <erickestradapj@gmail.com>
2023-06-21 21:45:12 -0500

    feat: new category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,21 +3,20 @@ import { useState } from 'react';
 export const GifExpertApp = () => {
    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-   console.log(categories);
+   const onAddCategory = () => {
+      setCategories([...categories, 'Valorant']);
+   };
 
    return (
       <>
          <h1>GifExpertApp</h1>
 
+         <button onClick={onAddCategory}>Add</button>
+
          <ol>
-            {categories.map((category) => {
-               return (
-                  <>
-                     <li key={category}>{category}</li>
-                  </>
-               );
+            {categories.map((category, index) => {
+               return <li key={index}>{category}</li>;
             })}
-            {/* <li>ABC</li> */}
          </ol>
       </>
    );
