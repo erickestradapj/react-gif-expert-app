530745999fbbab8ea95a7606342e76d1d85d6223
Erick Estrada <erickestradapj@gmail.com>
2023-06-21 22:49:16 -0500

    feat: GiftGrid component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
@@ -18,11 +19,9 @@ export const GifExpertApp = () => {
 
          <AddCategory onNewCategory={(value) => onAddCategory(value)} />
 
-         <ol>
-            {categories.map((category) => {
-               return <li key={category}>{category}</li>;
-            })}
-         </ol>
+         {categories.map((category) => (
+            <GifGrid key={category} category={category} />
+         ))}
       </>
    );
 };
