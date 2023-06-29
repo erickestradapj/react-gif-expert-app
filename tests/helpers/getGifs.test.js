22c594bad32e00fbc75e912e8bbc504af5bf4384
Erick Estrada <erickestradapj@gmail.com>
2023-06-29 12:58:34 -0500

    feat: test in getGifs

---
+++ tests/helpers/getGifs.test.js
@@ -0,0 +1,14 @@
+import { getGifs } from '../../src/helpers/getGifs';
+
+describe('test in getGif()', () => {
+   test('should return an array of numbers', async () => {
+      const gifs = await getGifs('One Punch');
+
+      expect(gifs.length).toBeGreaterThan(0);
+      expect(gifs[0]).toEqual({
+         id: expect.any(String),
+         title: expect.any(String),
+         url: expect.any(String),
+      });
+   });
+});
